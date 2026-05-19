465e590d2f4c7c72e777493569ecf2a3e46137a3
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-05-19 22:36:31 +0200

    Shared and customized the Settings screen

--- src/screens/settings-screen.ts
+++ src/screens/settings-screen.ts
@@ -1,64 +1,96 @@
 export const getSettingsScreenHTML = (): string => {
   return `
     <section class="settings-screen">
-        <h2>Settings</h2>
-
-        <div class="settings-layout-wrapper">
-            <div class="settings-controls">
-                <fieldset class="settings-group">
-                    <legend class="settings-label">Game themes</legend>
-                    <div class="options">
-                    <label class="option-item">
-                            <input type="radio" name="gameTheme" value="code-vibes" checked>
-                            <span>Code vibes theme</span>
-                        </label>
-                        <label class="option-item">
-                            <input type="radio" name="gameTheme" value="gaming">
-                            <span>Gaming theme</span>
-                        </label>
-                    </div>
-                </fieldset>
-
-                <fieldset class="settings-group">
-                    <legend class="settings-label">Choose player</legend>
-                    <div class="options">
-                    <label class="option-item">
-                            <input type="radio" name="chosenPlayer" value="blue" checked>
-                            <span>Blue</span>
-                        </label>
-                        <label class="option-item">
-                            <input type="radio" name="chosenPlayer" value="orange">
-                            <span>Orange</span>
-                        </label>
-                    </div>
-                </fieldset>
-
-                <fieldset class="settings-group">
-                    <legend class="settings-label">Board size</legend>
-                    <div class="options">
-                        <label class="option-item">
-                            <input type="radio" name="boardSize" value="16" checked>
-                            <span>16 cards</span>
-                        </label>
-                        <label class="option-item">
-                            <input type="radio" name="boardSize" value="24">
-                            <span>24 cards</span>
-                        </label>
-                        <label class="option-item">
-                            <input type="radio" name="boardSize" value="36">
-                            <span>36 cards</span>
-                        </label>
-                    </div>
-                </fieldset>
+        <div class="settings-content-box">
+            <div class="settings-title-group">
+                <h1 class="settings-title">Settings</h1>
+                <img class="settings-divider" src="./assets/icons/ui/line_1.svg" alt="" />
             </div>
 
-            <div class="preview-panel">
-                <img id="theme-preview" src="" alt="Theme Preview" />
-            </div>
-        </div>
+                <div class="settings-layout-wrapper">
+                    <div class="settings-controls">
+                        <fieldset class="settings-group">
+  
+                            <legend class="settings-label">
+                                <div class="legend-content">
+                                    <img class="settings-icon" src="./assets/icons/ui/settings_print.svg" alt="settings icon" />
+                                    <span>Choose player</span>
+                                </div>
+                            </legend>
+
+                            <div class="options">
+                                <label class="option-item">
+                                    <input type="radio" name="chosenPlayer" value="blue" checked>
+                                    <span class="custom-radio"></span>
+                                    <span>Blue</span>
+                                </label>
+                                <label class="option-item">
+                                    <input type="radio" name="chosenPlayer" value="orange">
+                                    <span class="custom-radio"></span>
+                                    <span>Orange</span>
+                                </label>
+                            </div>
+
+                        </fieldset>
+
+                        <fieldset class="settings-group">
+  
+                            <legend class="settings-label">
+                                <div class="legend-content">
+                                    <img class="settings-icon" src="./assets/icons/ui/chess_pawn.svg" alt="settings icon" />
+                                    <span>Choose player</span>
+                                </div>
+                            </legend>
+
+                            <div class="options">
+                                <label class="option-item">
+                                    <input type="radio" name="chosenPlayer" value="blue" checked>
+                                    <span class="custom-radio"></span>
+                                    <span>Blue</span>
+                                </label>
+                                <label class="option-item">
+                                    <input type="radio" name="chosenPlayer" value="orange">
+                                    <span class="custom-radio"></span>
+                                    <span>Orange</span>
+                                </label>
+                            </div>
+
+                        </fieldset>
+
+                        <fieldset class="settings-group">
+  
+                            <legend class="settings-label">
+                                <div class="legend-content">
+                                    <img class="settings-icon" src="./assets/icons/ui/settings_panel.svg" alt="settings icon" />
+                                    <span>Choose player</span>
+                                </div>
+                            </legend>
+
+                            <div class="options">
+                                <label class="option-item">
+                                    <input type="radio" name="chosenPlayer" value="blue" checked>
+                                    <span class="custom-radio"></span>
+                                    <span>Blue</span>
+                                </label>
+                                <label class="option-item">
+                                    <input type="radio" name="chosenPlayer" value="orange">
+                                    <span class="custom-radio"></span>
+                                    <span>Orange</span>
+                                </label>
+                            </div>
+
+                        </fieldset>
+                    </div>
+
+                    <div class="preview-panel">
+                        <img id="code-theme-preview" src="./assets/images/code_theme.svg"/>
+                        <!--<img id="gaming-theme-preview" src="./assets/images/gaming_theme.svg" alt="Gaming theme preview"/>-->
+                    </div>
+                </div>
 
             <!-- Der Button zum Starten unter dem Layout-Wrapper -->
             <button class="play-button" id="start-game-btn">Play</button>
+        </div>
     </section>
   `;
 };
