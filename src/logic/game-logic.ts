110c1fc16ec358b9f46c82c88db2c1137aa4321d
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-06-09 21:13:33 +0200

    fix: adjust match timeout to 550ms to prevent flickering

--- src/logic/game-logic.ts
+++ src/logic/game-logic.ts
@@ -136,7 +136,7 @@ function checkForMatch(allCards: Card[], render: () => void): void {
       } else {
         render();
       }
-    }, 400); 
+    }, 600); 
   } else {
     resetUnmatchedCards(card1, card2, render);
   }
